bf05f0036ecea3872138e86b8cdad3567130c248
jordansalinasp10 <jorasali@espol.edu.ec>
2024-11-09 22:36:49 -0500

    Primeras pruebas individuales

--- algoritmos/algoritmo_jordansalinasp10.rb
+++ algoritmos/algoritmo_jordansalinasp10.rb
@@ -1,29 +1,4 @@
-module Example
-  def self.calculate
-    a = 10
-    b = 20
-    unless a == b
-      a += 1
-      b **= 2
-      result = a % b
-    end
-    if a != b
-      puts "a is not equal to b"
-    end
-    range_inclusive = 1..10
-    range_exclusive = 1...10
-    if range_inclusive.include?(5)
-      puts "5 is in the inclusive range"
-    end
-    if range_exclusive.include?(10)
-      puts "10 is in the exclusive range"
-    end
-    case result
-    when 0
-      puts "Result is zero"
-    else
-      puts "Result is non-zero"
-    end
+end = "end"
     until result > 1000
       result *= 2
     end
@@ -32,9 +7,18 @@ module Example
   end
   def self.check_defined
     if defined? (@@CLASS_VARIABLEiable)
-      puts "Class variable is defined"
-    end
-  end
-end
-Example.calculate
-Example.check_defined
+
+
+mi_conjunto = Set.new([1, 2, 3, 4, 4])  # Los duplicados se eliminan automáticamente
+puts mi_conjunto.inspect  # Salida: #<Set: {1, 2, 3, 4}>
+# Agregar un elemento
+mi_conjunto.add(4)
+puts mi_conjunto.inspect  # Salida: #<Set: {1, 2, 3, 4}>
+
+# Agregar múltiples elementos
+mi_conjunto.merge([5, 6])
+puts mi_conjunto.inspect  # Salida: #<Set: {1, 2, 3, 4, 5, 6}>
+
+# Eliminar un elemento
+mi_conjunto.delete(3)
+puts mi_conjunto.inspect  # Salida: #<Set: {1, 2, 4, 5, 6}>
